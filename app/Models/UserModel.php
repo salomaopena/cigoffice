a5c1030c41c4cff6506c88397e9cb8316139cdb6
Salomão Pena <29301302+salomaopena@users.noreply.github.com>
2025-01-17 00:13:53 +0100

    Login and Logout implemented
    Login and Logout implemented

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -12,7 +12,7 @@ class UserModel extends Model
     protected $returnType       = 'object';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['last_login'];
 
     protected bool $allowEmptyInserts = false;
     protected bool $updateOnlyChanged = true;
@@ -23,6 +23,7 @@ class UserModel extends Model
     // Dates
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
+    //protected $lastLoginField = 'last_login';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
     protected $deletedField  = 'deleted_at';
@@ -43,4 +44,35 @@ class UserModel extends Model
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    public function check_for_login($email, $password, $restaurant_id)
+    {
+        //where clauses
+        $where = [
+            'email' => $email,
+            'id_restaurant' => $restaurant_id,
+            //'username' => $email,
+            'is_active' => 1, //User must be active
+            'blocked_until' => null, //User must be an administrator
+            'deleted_at ' => null //User must not be deleted
+            //add more conditions as needed
+        ];
+
+        $user = $this->where($where)->first();
+        //$user = $this->orWhere()->first();
+        //if user does not exist or password is incorrect, return false
+        if (empty($user)) {
+            return false;
+        }
+
+        if (!password_verify($password, $user->passwrd)) {
+            return false;
+        }
+        //User found, update last login date
+
+        $this->update($user->id, ['last_login' => date('Y-m-d H:i:s')]);
+
+        //return the user object
+        return $user;
+    }
 }
--- app/Views/auth/login-form.php
+++ app/Views/auth/login-form.php
@@ -1,13 +1,14 @@
-<<?= $this->extend('layouts/layout-auth') ?>
+<?= $this->extend('layouts/layout-auth') ?>
 
-    <?= $this->section('content') ?>
+<?= $this->section('content') ?>
 
 
-    <div class="login-box">
+<div class="login-box">
 
     <div class="text-center mb-3">
         <img class="img-fluid" src="<?= base_url('assets/images/logo.png') ?>" alt="logo" width="100">
     </div>
+
 
 
     <?= form_open('auth/login-submit', ['novalidate' => true]) ?>
@@ -20,7 +21,7 @@
                 if (!empty($restaurant_selected) && $restaurant_selected == $restaurant->id) {
                     $selected = 'selected';
                 } ?>
-                <option value="<?=Encrypt($restaurant->id)?>"<?=$selected?>><?= $restaurant->name ?></option>
+                <option value="<?= Encrypt($restaurant->id) ?>" <?= $selected ?>><?= $restaurant->name ?></option>
             <?php endforeach ?>
         </select>
         <?= display_error('restaurant_selected', $validation_errors) ?>
@@ -37,15 +38,19 @@
         <button type="submit" class="btn btn-primary btn-block btn-login">Login</button>
     </div>
     <?= form_close() ?>
+
+    <?php if (!empty($login_error)): ?>
+        <div class="alert alert-danger text-center p-1" role="alert">
+            <?= $login_error ?>
+        </div>
+    <?php endif ?>
+
     <div class="text-center mt-1 login-link">
         <p class="text-muted">Esqueceu a senha? <a href="#">Recuperar</a></p>
     </div> <!-- fim-->
     <!-- criar conta-->
     <div class="text-center mt-1 login-link">
         <p class="text-muted">Não possui conta? <a href="login.html">Registar-se</a></p>
-
-        <!-- script for password strength indicator-->
-        <script src="password-strength-indicator.js"></script>
     </div> <!-- fim-->
 
     <div class="text-center mt-2 login-link">
@@ -53,6 +58,66 @@
                 de
                 Uso</a> e <a href="#">Política de Privacidade</a></p>
     </div>
-    </div>
+</div>
 
-    <?= $this->endSection() ?>
+
+<!-- Bootstrap JS -->
+<script>
+    let wrapper = document.querySelector('.login-box');
+    let loginData = [
+        {
+            email: 'admin1@email.com',
+            password: 'user1',
+            restaurant: 1,
+        },
+        {
+            email: 'user1@email.com',
+            password: 'user1',
+            restaurant: 1,
+        },
+        {
+            email: 'admin2@email.com',
+            password: 'admin2',
+            restaurant: 2,
+        },
+        {
+            email: 'user2@email.com',
+            password: 'user2',
+            restaurant: 2,
+        },
+        {
+            email: 'admin3@email.com',
+            password: 'admin3',
+            restaurant: 3,
+        },
+        {
+            email: 'user3@email.com',
+            password: 'user3',
+            restaurant: 3,
+        }
+        
+    ];
+    const select = document.createElement('select');
+    select.appendChild(document.createElement('option'))
+    select.setAttribute('name','select_login');
+    loginData.forEach((item, index)=>{
+        let option = document.createElement('option');
+        option.setAttribute('value',index);
+        option.innerHTML = `Restaurante ${item.restaurant} -> ${item.email}`
+        select.appendChild(option);
+    });
+    wrapper.appendChild(select);
+    select.addEventListener('change', function(e){
+        const index = e.target.value;
+        if (index=='') return;
+        const restaurant = loginData[index].restaurant;
+        const email = loginData[index].email;
+        const password = loginData[index].password;
+
+        document.querySelector('#restaurant_selected').selectedIndex = restaurant
+        document.querySelector('#email').value = email
+        document.querySelector('#password').value = password
+    });
+</script>
+
+<?= $this->endSection() ?>
