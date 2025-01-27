c2066b52ee7b2b6d2867a4ddbe7d1c971222f07b
Salomão Pena <29301302+salomaopena@users.noreply.github.com>
2025-01-27 21:40:03 +0100

    Creted stock model
    Creted stock model

---
+++ app/Database/Migrations/2025-01-26-213514_StocksTable.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class StocksTable extends Migration
+{
+    public function up()
+    {
+        // Create stocks table
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'id_product' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            'stock_quantity' => [
+                'type' => 'INT',
+                'constraint' => 11,
+            ],
+            'stock_in_out' => [
+                'type' => 'VARCHAR',
+                'constraint' => 5,  // IN or OUT
+                'null' => true,
+            ],
+            'stock_supplier' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'null' => true,
+            ],
+            'reason' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        // Add primary key
+        $this->forge->addKey('id', true);
+
+        // Create stocks table
+        $this->forge->createTable('stocks');
+    }
+
+    public function down()
+    {
+        //
+        $this->forge->dropTable('stocks');
+    }
+}
+
